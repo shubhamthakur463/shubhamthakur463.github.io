e0aa583d5d4fe0ec66ec7cc22af018315078df21
Shubham Thakur <thakur.shubham463@gmail.com>
2020-12-15 17:28:23 -0500

    Implemented Responsive Top Navigation Bar

---
+++ js/app.js
@@ -0,0 +1,14 @@
+/* Overlay Menu for Mobile Devices */
+
+/* Open  */
+function openMenuNav() {
+    document.getElementById("overlayMenuNav").style.height = "100%";
+    document.getElementById("hide").style.visibility = "hidden";
+    
+}
+
+/* Close */
+function closeMenuNav() {
+    document.getElementById("overlayMenuNav").style.height = "0%";
+    document.getElementById("hide").style.visibility = "visible";
+}
